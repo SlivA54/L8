5becc08fc8815d78d643cb22bcdf57fa12587da1
SlivA54 <gavrisevdanil765@gmail.com>
2025-12-16 21:44:32 +0700

    3

--- app/src/main/java/com/example/l8/MainActivity.kt
+++ app/src/main/java/com/example/l8/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.l8.ui.theme.L8Theme
 
 class MainActivity : ComponentActivity() {
@@ -30,19 +37,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    // Здесь будут демо-компоненты
+    ClickDemo(modifier)
 }
 
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GestureDemoTheme {
-        MainScreen()
+fun ClickDemo(modifier: Modifier = Modifier) {
+    var colorState by remember { mutableStateOf(true) }
+    var bgColor by remember { mutableStateOf(Color.Blue) }
+
+    val clickHandler = {
+        colorState = !colorState
+        bgColor = if (colorState) Color.Blue else Color.DarkGray
     }
+
+    Box(
+        modifier
+            .clickable { clickHandler() }
+            .background(bgColor)
+            .size(100.dp)
+    )
 }
 
-@Composable
-fun GestureDemoTheme(content: @Composable () -> Unit) {
-    TODO("Not yet implemented")
-}
